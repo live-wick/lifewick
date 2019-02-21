c98787c890072d65a6ad5cb5b683caf1a2524927
HAMZA AZHAR <hamza@intagleo.com>
2019-02-21 16:34:40 +0500

    Added host in application yml file for swagger

--- config/initializers/swagger_docs.rb
+++ config/initializers/swagger_docs.rb
@@ -12,7 +12,7 @@ Swagger::Docs::Config.register_apis({
     # the output location where your .json files are written to
     :api_file_path => "public/apidocs",
     # the URL base path to your API
-    :base_path => "http://localhost:3000",
+    :base_path => "#{ENV['SWAGGER_HOST']}",
     # if you want to delete all .json files at each generation
     :clean_directory => true,
     # add custom attributes to api-docs
