b8476aba0e451a9a24439e35342708bbb678b5c3
HAMZA AZHAR <hamza@intagleo.com>
2019-04-01 13:17:52 +0500

    Reset Password API

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -1,15 +1,35 @@
 # frozen_string_literal: true
 
 class Users::PasswordsController < Devise::PasswordsController
+  skip_before_action :verify_authenticity_token, only: [:create, :edit]
+  skip_before_action :require_no_authentication, only: [:create, :edit, :update]
+
   # GET /resource/password/new
   # def new
   #   super
   # end
 
   # POST /resource/password
-  # def create
-  #   super
-  # end
+  def create
+    begin
+      self.resource = resource_class.send_reset_password_instructions(params)
+      yield resource if block_given?
+
+      if successfully_sent?(resource)
+        render json: {success: true, error: false, message: "You will receive an email with instructions on how to reset your password in a few minutes."}, status: 200
+        # respond_to do |format|
+        #   format.html {respond_with({}, location: after_sending_reset_password_instructions_path_for(resource_name))}
+        #   format.json { 
+        #   }
+        # end
+        
+      else
+        render json: {success: false, error: true, message: resource.errors.full_messages.join(', ')}, status: 422
+      end
+    rescue Exception => e
+      render json: {success: false, error: true, message: e}, status: 500
+    end
+  end
 
   # GET /resource/password/edit?reset_password_token=abcdef
   # def edit
